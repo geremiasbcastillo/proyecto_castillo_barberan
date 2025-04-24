0dd0a9d1513bf80862cc74c25a7e35db60a16a92
Benjamin <geremiasbcastillo@gmail.com>
2025-04-23 21:46:28 -0300

    Segunda correcion del nav

--- app/Views/plantillas/nav_view.php
+++ app/Views/plantillas/nav_view.php
@@ -1,15 +1,15 @@
 <nav class="navbar navbar-expand-lg">
-  <div class="container">
+  <div class="container-fluid">
     <a class="navbar-brand" href="inicio"><?php echo '<img src="assets/img/logo.png" class ="logo";>'?></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
         <li class="nav-item mx-3 pl-3">
           <a class="nav-link boton-nav active" aria-current="page" href="#">Inicio</a>
         </li>
-        <li class="nav-item mx-3 pl-3">
+        <li class="nav-item mx-3 pl-3 ">
           <a class="nav-link boton-nav" href="nosotros">Nosotros</a>
         </li>
         <li class="nav-item mx-3 pl-3 dropdown">
