8af04fa393c5797025e861a9c5b0eb7bd346a2bc
Andres <andresbarberan03@gmail.com>
2025-04-28 14:12:27 -0300

    nuevo comienzo de nosotros

--- app/Views/frontend/nosotros_view.php
+++ app/Views/frontend/nosotros_view.php
@@ -2,9 +2,38 @@
 <html lang="en">
 
 <body>
-    <div class="wrapper">
+    <section>
+        <div class="contenedor-fondo">
+            <div class="overlay"></div>
+            <div class="texto-superpuesto">
+                <h1 class="titulo">NOSOTROS</h1>
+                <p style="font-size: 20px">Conocenos, somos el mejor partido antes de jugarlo.</p>
+            </div>
+        </div>
+    </section>
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-4">
+                <h2>Nuestra historia</h2>
+                <p style="font-">Hace 20 años, en pleno paso a la adultez y sin mucho que hacer, nos dimos cuenta
+                    de que todavía no había ninguna página centrada 100% en el fútbol. De esta
+                    manera, decidimos que nosotros sentaríamos las bases. Orientada al fútbol,
+                    creamos la mejor tienda del deporte más popular del mundo y, sobre todo, de nuestro país, Argentina.
+                </p>
+            </div>
+            <div class="col-4">
+                <h2>Que te ofrecemos?</h2>
+                En nuestra página vas a encontrar todo para disfrutar de un partido, ya sea como jugador, entrenador
+                o incluso hincha. Para obtener nuestros productos, podés acercarte a cualquiera de nuestras más de
+                30 sucursales en todo el país, o hacerlo desde la comodidad de tu casa, y te lo enviamos estés donde
+                estés, siempre con la mejor atención y seguimiento.
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="wrapper">
     <div class="contenedor-fondo">
-        <div class="overlay"></div> <!-- Capa superpuesta -->
+        <div class="overlay"></div>
         <div class="texto-superpuesto">
             <h1 class="titulo">NOSOTROS</h1>
             <p style="font-size: 20px">Conocenos, somos el mejor partido antes de jugarlo.</p>
@@ -37,5 +66,5 @@
             </div>
         </div>
         </div>
-    </div>
+    </div> -->
 </body>
