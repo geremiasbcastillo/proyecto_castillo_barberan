fb487528c8b4d53d55139bec5e98fe469d6d26eb
Benjamin <geremiasbcastillo@gmail.com>
2025-04-28 21:52:27 -0300

    Correción de imagenes pertinentes

--- app/Views/frontend/camisetas_view.php
+++ app/Views/frontend/camisetas_view.php
@@ -6,7 +6,7 @@
 <div class="row row-col-1 g-4 mt-5 cards">
   <div class="col">
     <div class="card h-100">
-      <img src="assets/img/galeria1.jpg" class="card-img-top" alt="...">
+    <?php echo '<img src="assets/img/galeria1.png" class="card-img-top" alt="...">';?>
       <div class="card-body">
         <h5 class="card-title">Boca Juniors - 2000</h5>
         <p class="card-text">Camiseta usada por Boca Juniors en la final intercontinental ante el Real Madrid.</p>
@@ -16,7 +16,7 @@
   </div>
   <div class="col">
     <div class="card h-100">
-      <img src="assets/img/galeria2.png" class="card-img-top" alt="...">
+    <?php echo '<img src="assets/img/galeria2.png" class="card-img-top" alt="...">';?>
       <div class="card-body">
         <h5 class="card-title">Inter de Milan - 2010</h5>
         <p class="card-text">Esta camiseta fue usada por el Inter de Milán durante la Champions League 2009/2010, donde salieron campeones.</p>
@@ -26,7 +26,7 @@
   </div>
   <div class="col">
     <div class="card h-100">
-      <img src="assets/img/camiseta-argentina-2.png" class="card-img-top" alt="...">
+    <?php echo '<img src="assets/img/camiseta-argentina-2.png" class="card-img-top" alt="...">';?>
       <div class="card-body">
         <h5 class="card-title">Selección Argentina - 2022</h5>
         <p class="card-text">Camiseta con la cual la Selección Argentina se coronó campeona del mundial de Qatar.</p>
