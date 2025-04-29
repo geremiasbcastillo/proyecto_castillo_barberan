3246905eadb0c9fbd69a0c8e6c2a76877bec5b29
Andres <andresbarberan03@gmail.com>
2025-04-28 21:06:16 -0300

    fin

--- app/Views/frontend/nosotros_view.php
+++ app/Views/frontend/nosotros_view.php
@@ -38,41 +38,4 @@
                 <p class="objetivo" style="font-size: 25px">Constantemente nos estamos capacitando para darte la mejor atención.</p>
     </div>
     </section>
-
-    <!-- <div class="wrapper">
-    <div class="contenedor-fondo">
-        <div class="overlay"></div>
-        <div class="texto-superpuesto">
-            <h1 class="titulo">NOSOTROS</h1>
-            <p style="font-size: 20px">Conocenos, somos el mejor partido antes de jugarlo.</p>
-        </div>
-    </div>
-    <section class="container-fluid">
-        <div class="contenedor row row-col-1 row-col-md-2 g-4">
-            <div class="texto-izquierdo col">
-                <h2 class="titulo">Nuestra historia</h2>
-                <p>Hace 20 años, en pleno paso a la adultez y sin mucho que hacer, nos dimos cuenta
-                    de que todavía no había ninguna página centrada 100% en el fútbol. De esta
-                    manera, decidimos que nosotros sentaríamos las bases. Orientada al fútbol,
-                    creamos la mejor tienda del deporte más popular del mundo y, sobre todo, de nuestro país, Argentina.
-                </p>
-            </div>
-            <div class="texto-derecho col">
-                <h2 class="titulo">¿Qué te ofrecemos?</h2>
-                <p>En nuestra página vas a encontrar todo para disfrutar de un partido, ya sea como jugador, entrenador
-                    o incluso hincha. Para obtener nuestros productos, podés acercarte a cualquiera de nuestras más de
-                    30 sucursales en todo el país, o hacerlo desde la comodidad de tu casa, y te lo enviamos estés donde
-                    estés, siempre con la mejor atención y seguimiento.</p>
-            </div>
-        </div>
-    </section>
-        <div class="contenedor-equipo">
-        <div class="overlay-1">
-            <div class="texto-superpuesto">
-                <h1 class="titulo-1">Nuestro objetivo</h1>
-                <p class="objetivo">Constantemente nos estamos capacitando para darte la mejor atención.</p>
-            </div>
-        </div>
-        </div>
-    </div> -->
 </body>
