54a03f31a0e61f35149b7e295d52f6df71fd0204
Andres <andresbarberan03@gmail.com>
2025-06-16 15:42:13 -0300

    Estilos a paginas de administrador

--- app/Views/backend/agregar_producto_view.php
+++ app/Views/backend/agregar_producto_view.php
@@ -1,5 +1,5 @@
 <?php helper('form'); ?>
-<h1 class="text-center"> Registro de productos</h1>
+
 <div class="container">
     <div class="w-50 mx-auto">
         <?php if(!empty($validation)): ?>
@@ -18,12 +18,13 @@
             </div>
         <?php }?>
 
-        <?php echo form_open_multipart('agregar_producto') ?>
-            <div class="form-group">
+        <?php echo form_open_multipart('agregar_producto', ['class' => 'formulario-agregar-producto']) ?>
+        <h2 class="text-center"> Registro de productos</h1>
+            <div class="form-input">
                 <label for="nombre">Nombre del producto</label>
                 <?php echo form_input(['name' => 'nombre', 'id'=>'nombre', 'type'=>'text', 'class' => 'form-control', 'placeholder' => 'Ingrese el nombre del producto', 'value' => set_value('nombre')]); ?>
             </div>
-            <div class="form-group">
+            <div class="form-input">
                 <label for="descripcion">Descripcion del producto</label>
                 <?php echo form_input(['name' => 'descripcion', 'id'=>'descripcion', 'type'=>'text', 'class' => 'form-control', 'placeholder' => 'Ingrese la descripcion del producto', 'value' => set_value('descripcion')]); ?>
             </div>    
@@ -39,19 +40,19 @@
                     echo form_dropdown('categoria', $lista, '0', 'class = "form-control"');
                 ?>
             </div>
-            <div class="form-group">
+            <div class="form-input">
                 <label for="precio">Precio del producto</label>
                 <?php echo form_input(['name' => 'precio', 'id'=>'precio', 'type'=>'number', 'class' => 'form-control', 'placeholder' => 'Ingrese el precio del producto', 'value' => set_value('precio')]); ?>
             </div>
-            <div class="form-group">
+            <div class="form-input">
                 <label for="cantidad">Cantidad del producto</label>
                 <?php echo form_input(['name' => 'cantidad', 'id'=>'cantidad', 'type'=>'number', 'class' => 'form-control', 'placeholder' => 'Ingrese la cantidad del producto', 'value' => set_value('cantidad')]); ?>
             </div>
-            <div class="form-group">
+            <div class="form-input">
                 <label for="imagen">Imagen del producto</label>
                 <?php echo form_input(['name' => 'imagen', 'id'=>'imagen', 'type'=>'file', 'class' => 'form-control', 'value'=>set_value('imagen')]); ?>
             </div>
-            <div class="form-group mt-3">
+            <div class="form-input mt-3">
                 <?php echo form_submit('Agregar', 'Agregar', "class='btn btn-success'"); ?>
             </div>
             
