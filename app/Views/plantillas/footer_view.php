9a7bc736afada7506fd41dfe7886db5d37ef6d56
Andres <andresbarberan03@gmail.com>
2025-06-18 13:53:37 -0300

    Cambio header al footer

--- app/Views/plantillas/footer_view.php
+++ app/Views/plantillas/footer_view.php
@@ -1,22 +1,46 @@
+<footer>
+  <div class="container text-center pb-3 pt-3">
+    <div class="row justify-content-md-center">
+      <div class="col-md-4 col-sm-12">
+        <h5>Información:</h5>
+        <div class="p-1">Dirección: 9 de julio 1449, Corrientes, Argentina</div>
+        <div class="p-1">Email: atencionalcliente@doblecinco.com.ar</div>
+        <div class="p-1">Teléfono: +54 9 379 - 4437486</div>
+      </div>
 
-<footer class="text-center text-lg-start mt-3">
-  <!-- Copyright -->
-  <div class="container-fluid mt-2">
-    <div class="col text-center">
+      <div class="logos col-md-4 col-sm-12 align-self-center mb-3">
+        <a href="https://www.facebook.com/">
+          <img src="<?= base_url('assets/img/facebook.svg') ?>" height="25px">
+        </a>
+        <a href="https://www.tiktok.com/">
+          <img src="<?= base_url('assets/img/tiktok.svg') ?>" height="25px">
+        </a>
+        <a href="https://www.instagram.com/">
+          <img src="<?= base_url('assets/img/instagram.svg') ?>" height="25px">
+        </a>
+      </div>
+
+    <div class="col-md-4 col-sm-12 align-self-center">
+      <div class="col text-center">
       <a href="terminos" class="text-body ">Terminos y Usos</a>
     </div>
     <div class="col">
-      <div class="text-center p-3">
+      <div class="text-center">
         © 2025 Copyright:
         <a class="text-body" href="#">Castillo - Barberan</a>
       </div>
     </div>
+    </div>
+
+    </div>
   </div>
-  
-  <!-- Copyright -->
 </footer>
 
+<link href="<?= base_url('assets/css/miestilo.css') ?>" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://kit.fontawesome.com/ce6c7e6ad7.js" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
+
 </html>
